d1dc14cd8fe43e97c7bb1ba52831e483985d96bd
rsoleyma <ramin.soleymani@bsc.es>
2024-09-13 10:58:40 +0200

    store to db, make some status updates more config options

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import gzip
 import io
 import json
 import tarfile
+from datetime import datetime
 from pathlib import Path
 from typing import Generator
 
 import jsonlines
-import orjson
 from tqdm import tqdm
 
-from consts import CONFIG, STATUS_FILE, BASE_PROCESS_PATH
-from db_config import init_db
+from consts import CONFIG, STATUS_FILE, BASE_PROCESS_PATH, logger
+from db_config import init_db, Session
 from db_models import DBPost
 
 
@@ -38,32 +38,77 @@ def iter_jsonl_files_data(tar_file: Path) -> Generator[tuple[str, bytes], None, 
                 with gzip.GzipFile(fileobj=io.BytesIO(extracted_file.read())) as gz_bytes:
                     yield member.name, gz_bytes.read()
 
-def dump2db(data: dict) -> None:
-    pass
+
+def create_db_entry(data: dict) -> DBPost:
+    post_dt = datetime.strptime(data["created_at"], '%a %b %d %H:%M:%S %z %Y')
+    post = DBPost(
+        platform="twitter",
+        post_url_computed=f"https://x.com/x/status/{data['id']}",
+        date_created=post_dt,
+        content=data if CONFIG.STORE_COMPLETE_CONTENT else None,
+        text=data["text"],
+        language=data["lang"]
+    )
+    post.set_date_columns()
+    return post
+
 
 def process_dump(dump_path: Path):
     assert dump_path.name.startswith("archiveteam-twitter-stream")
     dump_file_date = dump_path.name.lstrip("archiveteam-twitter-stream")
+    logger.info(f"dumping: {dump_file_date}")
     dump_file_status = {}
     status[dump_file_date] = dump_file_status
     dump_dir = BASE_PROCESS_PATH / dump_file_date
     dump_dir.mkdir(exist_ok=True)
+    session = Session()
     for tar_file in dump_path.glob("twitter-stream-*.tar"):
         tar_date_name = tar_file.name.lstrip("twitter-stream-").rstrip(".tar")
+        logger.info(f"tar_file: {tar_date_name}")
         tar_file_status = {}
         dump_file_status[tar_date_name] = tar_file_status
         for jsonl_file_name, jsonl_file_data in iter_jsonl_files_data(tar_file):
             entries_count = 0
-            for jsonl_entry in jsonlines.Reader(io.BytesIO(jsonl_file_data)):
+            accepted = 0
+            earliest_dt = None
+            latest_dt = None
+            json_file_status = {}
+            tar_file_status[jsonl_file_name] = json_file_status
+            for jsonl_entry in (jsonlines.Reader(io.BytesIO(jsonl_file_data))):
                 entries_count += 1
                 if "data" in jsonl_entry:
                     data = jsonl_entry["data"]
-                else: # 2022-01
+                else:  # 2022-01
                     data = jsonl_entry
-                if (data.get("referenced_tweets") is None or not CONFIG.ONLY_ORIG_TWEETS) and data.get("lang") in CONFIG.LANGUAGES:
-                    print(data)
-                    dump2db(jsonl_entry)
+                if (((data.get("referenced_tweets") is None and data.get(
+                        "in_reply_to_status_id") is None
+                      and data.get("quoted_status_id") is None and data.get(
+                            "retweeted_status")) or not CONFIG.ONLY_ORIG_TWEETS)
+                        and data.get("lang") in CONFIG.LANGUAGES):
+                    # print(data)
+                    accepted += 1
+                    logger.debug(f"{accepted} / {entries_count}")
+                    db_post = create_db_entry(jsonl_entry)
+                    # setup earliest and latest
+                    if not earliest_dt:
+                        earliest_dt = db_post.date_created
+                        latest_dt = db_post.date_created
+                    else:
+                        if db_post.date_created < earliest_dt:
+                            earliest_dt = db_post.date_created
+                        if db_post.date_created > latest_dt:
+                            latest_dt = db_post.date_created
+                    session.add(db_post)
+                    if len(session.new) > CONFIG.DUMP_THRESH:
+                        session.commit()
+                    pass
                     # yield data
+            json_file_status.update(
+                {"entries_count": entries_count,
+                 "accepted": accepted,
+                 "earliest_dt": earliest_dt.strftime("%Y.%m.%d:%H:%M:%S"),
+                 "latest_dt": latest_dt.strftime("%a %b %d %H:%M:%S %z %Y")
+                 })
         break
 
 
@@ -73,7 +118,6 @@ def iter_tar_files(path: Path) -> Generator[Path, None, None]:
 
 def list_tar_gz_files(path: Path) -> Generator[Path, None, None]:
     return path.glob("*.tar.gz")
-
 
 
 def exit_handler():
