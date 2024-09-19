73987bbebe0b80b3cac3bcfd874f187dda7aaff5
rsoleyma <ramin.soleymani@bsc.es>
2024-09-19 13:19:38 +0200

    pick data allows batching

--- pick_data.py
+++ pick_data.py
@@ -14,9 +14,11 @@ from sqlalchemy.orm import DeclarativeBase
 
 from consts import CONFIG
 
-# not sure this is needed
+# not sure if this is needed
 T = TypeVar('T', bound=DeclarativeBase)
+
 ZipFormat = Literal["bz2", "gzip"]
+
 
 def extract_member_in_tar_file(tar_file: Path, member_name: str) -> bytes:
     with tarfile.open(tar_file, 'r') as tar:
@@ -36,26 +38,63 @@ def unpack(bytes_data: bytes, compression_type: ZipFormat, name: str):
             return gz_bytes.read()
 
 
-
 def grab_post_from_location(location_index: tuple[str, str, str, int]) -> dict:
-    p = CONFIG.STREAM_BASE_FOLDER / f"archiveteam-twitter-stream-{location_index[0]}"
+    dump_file_date_name, tar_file_date_name, jsonl_file_name, jsonl_line = location_index
+
+    p = CONFIG.STREAM_BASE_FOLDER / f"archiveteam-twitter-stream-{dump_file_date_name}"
     if not p.exists():
         raise FileNotFoundError(f"{p} does not exist")
-    tar_file_path = p / f"twitter-stream-{location_index[1]}.tar"
+    tar_file_path = p / f"twitter-stream-{tar_file_date_name}.tar"
     if not tar_file_path.exists():
         raise FileNotFoundError(f"{tar_file_path} does not exist")
 
-    compressed_data: bytes = extract_member_in_tar_file(tar_file_path, location_index[2])
-    compression_type = Path(location_index[2]).suffix[1:]
+    compressed_data: bytes = extract_member_in_tar_file(tar_file_path, jsonl_file_name)
+    compression_type = Path(jsonl_file_name).suffix[1:]
     assert compression_type
-    jsonl_str_data = unpack(compressed_data, cast(ZipFormat, compression_type), location_index[2])
+    jsonl_str_data = unpack(compressed_data, compression_type=cast(ZipFormat, compression_type), name=jsonl_file_name)
     lines = jsonl_str_data.decode("utf-8").split("\n")
     # alternatively try bot_suggestions.jsonl_iterator
     # print(jsonl_str_data)
-    return json.loads(lines[location_index[3]])
+    return json.loads(lines[jsonl_line])
+
+
+def grab_posts_from_location(location_index: tuple[str, str, list[str], list[list[int]]]) -> list[dict]:
+    """
+    can grab many data from one tar file
+    :param location_index:
+    :return:
+    """
+    # these 2 parts are just a path and could be joined
+    dump_path, tar_file_date_name, jsonl_file_names, jsonl_lines = location_index
+
+    p = CONFIG.STREAM_BASE_FOLDER / f"archiveteam-twitter-stream-{dump_path}"
+    if not p.exists():
+        raise FileNotFoundError(f"{p} does not exist")
+    tar_file_path = p / f"twitter-stream-{tar_file_date_name}.tar"
+    if not tar_file_path.exists():
+        raise FileNotFoundError(f"{tar_file_path} does not exist")
+
+    data: list[dict] = []
+
+    if len(jsonl_file_names) != len(jsonl_lines):
+        raise ValueError(
+            f"Length of number of tar files must match length of last part (jsonl files for each tar file)")
+    for jsonl_file_idx, jsonl_file_name in enumerate(jsonl_file_names):
+        compressed_data: bytes = extract_member_in_tar_file(tar_file_path, jsonl_file_name)
+        compression_type = Path(jsonl_file_name).suffix[1:]
+        assert compression_type
+        jsonl_str_data = unpack(compressed_data, compression_type=cast(ZipFormat, compression_type),
+                                name=jsonl_file_name)
+        lines = jsonl_str_data.decode("utf-8").split("\n")
+        for jsonl_idx in jsonl_lines[jsonl_file_idx]:
+            data.append(json.loads(lines[jsonl_idx]))
+    # alternatively try bot_suggestions.jsonl_iterator
+    # print(jsonl_str_data)
+    return data
 
 
 if __name__ == '__main__':
     # thats a test...
-    data = grab_post_from_location(("2022-03", "20220301", "20220301/20220301233400.json.gz", 3))
+    #data = grab_post_from_location(("2022-03", "20220301", "20220301/20220301233400.json.gz", 3))
+    data = grab_posts_from_location(("2022-03", "20220301", ["20220301/20220301233400.json.gz"], [[3,4,5]]))
     print(json.dumps(data, indent=2))
