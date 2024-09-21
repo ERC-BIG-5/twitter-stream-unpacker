76c4b806397fd952e9572c1ad4fab493640dce69
rsoleyma <ramin.soleymani@bsc.es>
2024-09-21 14:06:24 +0200

    started with create_anon_entries.py

--- src/create_anon_entries.py
+++ src/create_anon_entries.py
@@ -1,12 +1,40 @@
+import json
+from datetime import datetime
+
 from collect_from_time_range_table import get_first_tweets_by_hour
+from consts import BASE_DATA_PATH
 from db import TimeRangeEvalEntry, DBAnnot1Post
 
-def create_annot1__from_time_range_posts(posts: list[TimeRangeEvalEntry]) -> list[DBAnnot1Post]:
+def check_contains_media(post: dict)-> bool:
     pass
 
+def create_annot1__from_time_range_posts(posts: list[dict]) -> list[DBAnnot1Post]:
+
+    for post in posts:
+        db_post = DBAnnot1Post(
+            post_url=f"x.com/{post['user']['name']}/status/{post['id']}",
+            location_index=[], # todo, pass with the rest
+            platform_id=post['id_str'],
+            date_created=datetime.fromtimestamp(int(post['timestamp_ms'] / 1000)),
+            language=post['lang'],
+            text=post['text'],
+            contain_media=""
+        )
+        db_post.set_date_columns()
+    """
+            platform="twitter",
+        post_url_computed=f"https://x.com/x/status/{data['id']}",
+        date_created=post_dt,
+        language=data["lang"],
+        location_index=location_index,
+    """
+
 def main_create_annot1_db():
-    posts = get_first_tweets_by_hour(2022,1,{"en"},1)
-    pass
+    # posts = get_first_tweets_by_hour(2022,3,{"en"},1)
+    posts = json.load((BASE_DATA_PATH / "TEMP_MAR.json").open())
+    create_annot1__from_time_range_posts(posts)
+    #json.dump(posts, (BASE_DATA_PATH / "TEMP_MAR.json").open("w", encoding="utf-8"))
+    #pass
 
 if __name__ == "__main__":
     main_create_annot1_db()
