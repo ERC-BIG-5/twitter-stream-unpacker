9ebd530431cc3b2bbc5c49effeb09244ceb1d6cf
rsoleyma <ramin.soleymani@bsc.es>
2024-09-21 17:03:06 +0200

    create full entries... refactor

---
+++ src/post_filter.py
@@ -0,0 +1,12 @@
+from src.consts import CONFIG, logger
+
+
+def check_original_tweet(data: dict) -> bool:
+    is_orig =(data.get("referenced_tweets") is None and
+            data.get("in_reply_to_status_id") is None and
+            data.get("quoted_status_id") is None and
+            data.get("retweeted_status") is None) or not CONFIG.ONLY_ORIG_TWEETS
+
+    if is_orig and data["text"].startswith("RT"):
+        logger.warning("RT filter broken")
+    return is_orig
--- src/util.py
+++ src/util.py
@@ -1,14 +1,50 @@
+import bz2
+import gzip
+import io
+import tarfile
 from pathlib import Path
 
-from consts import logger
+from sqlalchemy.orm import DeclarativeBase
+
+from consts import logger, CONFIG
 from pathlib import Path
 from typing import Generator
 
 from jsonlines import jsonlines
 
 
+def get_dump_path(year: int, month: int)-> Path:
+    return CONFIG.STREAM_BASE_FOLDER / f"archiveteam-twitter-stream-{year}-{str(month).rjust(2,'0')}"
+
+
+def iter_tar_files(path: Path)-> Generator[Path, None, None]:
+    return path.glob("twitter-stream-*.tar")
+
 def list_jsonl_file(path: Path) -> Generator[Path, None, None]:
     return path.glob("*.jsonl")
+
+
+def tarfile_datestr(tar_file: Path) -> str:
+    return tar_file.name.lstrip("twitter-stream-").rstrip(".tar")
+
+
+
+def iter_jsonl_files_data(tar_file: Path) -> Generator[tuple[str, bytes], None, None]:
+    with tarfile.open(tar_file, 'r') as tar:
+        relevant_members = [member for member in tar.getmembers() if
+                            (member.name.endswith('.json.bz2') or member.name.endswith('.json.gz'))]
+        for member in relevant_members:
+            extracted_file = tar.extractfile(member)
+            if member.name.endswith("bz2"):
+                if extracted_file is not None:
+                    try:
+                        decompressed_data = bz2.decompress(extracted_file.read())
+                        yield member.name, decompressed_data
+                    except Exception as e:
+                        print(f"Error processing {member.name}: {str(e)}")
+            else:
+                with gzip.GzipFile(fileobj=io.BytesIO(extracted_file.read())) as gz_bytes:
+                    yield member.name, gz_bytes.read()
 
 
 def iter_jsonl_file(fp: Path) -> Generator[dict, None, None]:
@@ -22,9 +58,14 @@ def iter_jsonl_file(fp: Path) -> Generator[dict, None, None]:
             yield line
 
 
+
 def consider_deletion(path: Path):
     delete_resp = input(f"Do you want to delete the file"
                         f"{path}? : y/ other key\n")
     if delete_resp == "y":
         logger.info(f"deleting: {path}")
         path.unlink()
+
+
+def post_url(data: dict)-> str:
+    return f"https://x.com/{data['user']['screen_name']}/status/{data['id']}"
